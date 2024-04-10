9df04374f0556ada428ef916a55b0cb592b349b6
eunbi <geunbi38@gmail.com>
2024-04-10 23:23:54 +0900

    Attribute마다 GameplayTag를 VS에서 직접 추가해서 VS C++ 코드 내에서도 태그 확인이 가능하도록 함.

---
+++ AG/Source/AG/AGGameplayTags.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AGGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAGGameplayTags FAGGameplayTags::mGameplayTags;
+
+void FAGGameplayTags::InitializeNativeGameplayTags()
+{
+
+	mGameplayTags.Attributes_Primary_Strength = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Primary.Strength"),
+		FString("Increase physical damage")
+	);
+	mGameplayTags.Attributes_Primary_Intelligence = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Primary.Intelligence"),
+		FString("Increase megical damage")
+	);
+	mGameplayTags.Attributes_Primary_Resilience = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Primary.Resilience"),
+		FString("Increase Armor")
+	);
+	mGameplayTags.Attributes_Primary_Dexterity = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Primary.Dexterity"),
+		FString("Increase Block Percent")
+	);
+	mGameplayTags.Attributes_Primary_Coin = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Primary.Coin"),
+		FString("Coin")
+	);
+
+
+	mGameplayTags.Attributes_Second_MaxHp = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Second.MaxHp"), 
+		FString("max hp")
+	);
+	mGameplayTags.Attributes_Second_MaxMp = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Second.MaxMp"),
+		FString("max mp")
+	);
+	mGameplayTags.Attributes_Second_Armor = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Second.Armor"),
+		FString("Reduces damage")
+	);
+	mGameplayTags.Attributes_Second_BlockPercent = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Second.BlockPercent"),
+		FString("Percent to reduce damage")
+	);
+	mGameplayTags.Attributes_Second_CriticalPercent = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Second.CriticalPercent"),
+		FString("Percent to double damage  plus hit")
+	);
+	mGameplayTags.Attributes_Second_CriticalDamage = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Second.CriticalDamage"),
+		FString("Bonus damage when critical scored")
+	);
+	mGameplayTags.Attributes_Second_CriticalResistance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Second.CriticalResistance"),
+		FString("Reduce Critical hit damage")
+	);
+
+}
---
+++ AG/Source/AG/AGGameplayTags.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "BasicInfo.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * 
+ */
+
+
+struct FAGGameplayTags
+{
+public:
+	static const FAGGameplayTags& Get() { return mGameplayTags; }
+	static void InitializeNativeGameplayTags();
+
+	FGameplayTag Attributes_Primary_Strength;
+	FGameplayTag Attributes_Primary_Intelligence;
+	FGameplayTag Attributes_Primary_Resilience;
+	FGameplayTag Attributes_Primary_Dexterity;
+	FGameplayTag Attributes_Primary_Coin;
+
+	FGameplayTag Attributes_Second_MaxHp;
+	FGameplayTag Attributes_Second_MaxMp;
+	FGameplayTag Attributes_Second_Armor;
+	FGameplayTag Attributes_Second_BlockPercent;
+	FGameplayTag Attributes_Second_CriticalPercent;
+	FGameplayTag Attributes_Second_CriticalDamage;
+	FGameplayTag Attributes_Second_CriticalResistance;
+
+protected:
+private:
+	static FAGGameplayTags mGameplayTags;
+};
